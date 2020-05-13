27a9a44b96a4600b7a172fa559a6254efbc01a43
Paul Rupke <paul.rupke@gmail.com>
2020-05-13 16:22:27 -0400

    Built hangman game and some tests

--- game/test/hangman_test.exs
+++ game/test/hangman_test.exs
@@ -12,5 +12,39 @@ defmodule GameTest do
         assert length(game.letters) > 0
         assert Enum.join(game.letters) =~ ~r/(^[^a-z]*)/
     end
-    
+
+    test "stat isn't changed for :won or :lost game" do
+        for state <- [ :won, :lost ] do
+            game = Game.new_game() |> Map.put(:game_state, :won)
+            assert { ^game, _ } = Game.make_move(game, "x")
+        end
+    end
+
+    test "first occurence of letter is not already used" do
+        game = Game.new_game()
+        { game, _tally } = Game.make_move(game, "x")
+        assert game.game_state != :already_used
+    end
+
+    test "second occurence of letter is not already used" do
+        game = Game.new_game()
+        { game, _tally } = Game.make_move(game, "x")
+        assert game.game_state != :already_used
+        { game, _tally } = Game.make_move(game, "x")
+        assert game.game_state == :already_used
+    end
+
+    test "a good guess is recognized" do
+        game = Game.new_game("whale")
+        {game, _tally } = Game.make_move(game, "w")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+    end
+
+    test "bad guess is recognized" do
+        game = Game.new_game("whale")
+        { game, _tally} = Game.make_move(game, "x")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 6
+    end
 end
