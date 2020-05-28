26d0838417edc16c2024a6539af0119cff9fb1b7
Paul Rupke <paul.rupke@gmail.com>
2020-05-28 16:47:20 -0400

    Added text_client, started main player

--- game/test/hangman_test.exs
+++ game/test/hangman_test.exs
@@ -13,62 +13,68 @@ defmodule GameTest do
         assert Enum.join(game.letters) =~ ~r/(^[^a-z]*)/
     end
 
-    test "stat isn't changed for :won or :lost game" do
+    test "state isn't changed for :won or :lost game" do
         for state <- [ :won, :lost ] do
             game = Game.new_game() |> Map.put(:game_state, :won)
-            assert { ^game, _ } = Game.make_move(game, "x")
+            assert ^game = Game.make_move(game, "x")
         end
     end
 
     test "first occurence of letter is not already used" do
         game = Game.new_game()
-        { game, _tally } = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
         assert game.game_state != :already_used
     end
 
     test "second occurence of letter is not already used" do
         game = Game.new_game()
-        { game, _tally } = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
         assert game.game_state != :already_used
-        { game, _tally } = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
         assert game.game_state == :already_used
     end
 
     test "a good guess is recognized" do
         game = Game.new_game("whale")
-        {game, _tally } = Game.make_move(game, "w")
+        game = Game.make_move(game, "w")
         assert game.game_state == :good_guess
         assert game.turns_left == 7
     end
 
     test "bad guess is recognized" do
         game = Game.new_game("whale")
-        { game, tally } = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
         assert game.game_state == :bad_guess
         assert game.turns_left == 6
     end
 
     test "lost game is recognized" do
         game = Game.new_game("w")
-        { game, _tally} = Game.make_move(game, "a")
+        game = Game.make_move(game, "a")
         assert game.game_state == :bad_guess
         assert game.turns_left == 6
-        { game, _tally} = Game.make_move(game, "b")
+        game = Game.make_move(game, "b")
         assert game.game_state == :bad_guess
         assert game.turns_left == 5
-        { game, _tally} = Game.make_move(game, "c")
+        game = Game.make_move(game, "c")
         assert game.game_state == :bad_guess
         assert game.turns_left == 4
-        { game, _tally} = Game.make_move(game, "d")
+        game = Game.make_move(game, "d")
         assert game.game_state == :bad_guess
         assert game.turns_left == 3
-        { game, _tally} = Game.make_move(game, "e")
+        game = Game.make_move(game, "e")
         assert game.game_state == :bad_guess
         assert game.turns_left == 2
-        { game, _tally} = Game.make_move(game, "f")
+        game = Game.make_move(game, "f")
         assert game.game_state == :bad_guess
         assert game.turns_left == 1
-        { game, _tally} = Game.make_move(game, "g")
+        game = Game.make_move(game, "g")
         assert game.game_state == :lost
+    end
+
+    test "guess is a single lowercase ASCII character" do
+        game = Game.new_game("test")
+        game = Game.make_move(game, "g")
+        assert "g" =~ ~r/(^[a-z]$)/
     end
 end
