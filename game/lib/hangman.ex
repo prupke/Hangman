47068e3dd705b60dd8fe354d37addc4efb6ef66c
Paul Rupke <paul.rupke@gmail.com>
2020-05-28 10:06:47 -0400

    CLI working, through 'write the hangman game' lesson

--- game/lib/hangman.ex
+++ game/lib/hangman.ex
@@ -3,8 +3,11 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(), to: Game
-   
-  def hello do
-    IO.puts Dictionary.random_word()
+  defdelegate tally(game), to: Game
+
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    { game, tally(game) }
   end
+
 end
